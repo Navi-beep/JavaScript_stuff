1ae8db21cde1c4b006fd4feafc0c18b26d61464b
Navi-Beep <estawars30@gmail.com>
2022-10-17 21:20:25 -0500

    changed index.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-let entry = prompt("Say hello there");
-if (entry == null || entry == "") {
-  alert("User left the chat.");
-} else {
-  alert("Hello there General Kenobi!");
-}
+//let entry = prompt("Say hello there");
+//if (entry == null || entry == "") {
+ // alert("User left the chat.");
+//} else {
+//  alert("Hello there General Kenobi!");
+//}
+
