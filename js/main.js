4faea39c97911e20ade8d036b741c47ce5593c9c
Navi-Beep <estawars30@gmail.com>
2023-02-09 18:52:21 -0600

    added function

--- js/main.js
+++ js/main.js
@@ -310,3 +310,35 @@ cats[1] = 'sphynx cat'
 
 console.log(cats)
 
+//Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+//For Example:
+
+//[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+//, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+//, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+//]
+//So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+
+function sumOfMinimums(arr) {
+    // your code here
+      let minSum = 0 
+      for(let i = 0; i< arr.length; i++){
+          let min = Math.min(...arr[i]);
+          minSum = minSum + min 
+      }
+      return minSum
+  }
+
+
+ console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])) 
+
+
+ let arr2 = [[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]
+
+ console.log(Math.min(...arr2[0]))
+ 
+
+ let arr3 = [1,2,3]
+ console.log(Math.min(...arr3))
